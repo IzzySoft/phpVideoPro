0a12d30c20edb7aa008fd6902a2d42caf42259a3
Izzy <izzysoft@qumran.org>
2001-06-22 13:33:56 +0000

    * updated to reflect recent state
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@10 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

--- about.php
+++ about.php
@@ -5,20 +5,12 @@
 
 ?>
 
-<table width="100%" border="0" cellspacing="0" cellpadding="0">
- <tr> 
-  <td width="7"><img src="<? echo $base_url ?>images/menu_bar_left.gif" width="7" height="24"></td>
-  <td background="<? echo $base_url ?>images/menu_bar_bg.gif" width="100%">&nbsp;</td>
-  <td width="9"><img src="<? echo $base_url ?>images/menu_bar_right.gif" width="9" height="24"></td>
- </tr>
-</table>
-
 <H2 Align=Center>About phpVideoPro</H2>
 
 <Table Width=90% Align=Center>
  <TR><TD>
   <P Align=Justify>This is phpVideoPro version <? echo $version ?>, (c) 2001 by
-   Itzchak Rehberg, all rights reserved. It's my try to port the best video
+   Itzchak Rehberg, all rights (and links) reserved. It's my try to port the best video
    management software I've ever found, VideoPro (c) Bernd Rickers, to a platform
    independant application, while adding some new features that were not to think
    of at that time (last available version of VideoPro is from 1994, so that time
@@ -28,13 +20,17 @@
    (as e.g. his phone book - when phpVideoPro is
  plugged into phpGroupWare, you'll
    have everything there :-)</P>
-  <P Align=Justify>So now I hope I will have some working version very soon. Right
-   now I kept the structure of the tables 100% compatible with Bernds Clipper
-   based stuff - as soon as phpVideoPro is able to replace VideoPros main
-   functions, I will no longer need VideoPro for editing my database and thus can
-   rework table structures, which I certainly will do. But that may take a while,
-   so stay tuned...</P>
-  <P Align=Right>Itzchak Rehberg, January 2001</P>
+  <P Align=Justify>So now I hope I will have some working version very soon. While
+   at the beginning (Version 0.0.1) I kept the structure of the tables 100% compatible
+   with Bernds Clipper
+based stuff, I changed that with version 0.0.2 to a structure
+   that seemed more suitable to me. So table structures as well as field names are
+   almost completely reworked by now, new tables are added, and more will come.
+   Starting with version 0.0.3 (which I hope to finnish before the summer break) there
+   will be a first usable program code, allowing to create new entries as well as
+   update/delete existing ones. But since this is only a hobby, I can't promise
+   anything - and just again ask you to stay tuned...</P>
+  <P Align=Right>Itzchak Rehberg, June 2001</P>
  </TD></TR>
 </Table>
 

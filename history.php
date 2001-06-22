2654cf2c61a46c94bba4e4f50181ba2ee9afb545
Izzy <izzysoft@qumran.org>
2001-06-22 13:33:30 +0000

    initial checkin of history page
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@9 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

---
+++ history.php
@@ -0,0 +1,36 @@
+<? /* About */
+
+  include("inc/config.inc");
+  include("inc/header.inc");
+
+?>
+
+<H2 Align=Center>phpVideoPro History</H2>
+
+<Table Width=90% Align=Center>
+ <TR><TD><UL>
+  <LI><b>0.0.2 (June 2001)</b><br>
+      database completely reworked and restructured to what I felt to be
+      suitable to fit the new requirements. Detail view largely improved.
+      New available functions:
+      <ul>
+       <li>improved Detail View (old one dropped :)
+       <li>possibility to change existing movie data
+       <li>added (this) history page
+      </ul>
+  <LI><b>0.0.1 (January 2001)</b><br>
+      initial version - just allowed browsing of existing data, no changes
+      are made to data. Features available:
+      <ul>
+       <li>MediaList
+       <li>Detail View
+       <li>About
+      </ul>
+ </UL></TD></TR>
+</Table>
+
+<?
+
+  include("inc/footer.inc");
+
+?>

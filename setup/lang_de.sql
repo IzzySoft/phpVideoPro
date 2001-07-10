f539e4d910ea0a69250825a53c9779fcb19c5a9b
Izzy <izzysoft@qumran.org>
2001-07-10 21:18:57 +0000

    introduced German translation file
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@64 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

---
+++ setup/lang_de.sql
@@ -0,0 +1,8 @@
+# ========================================================
+# English Translation phrases for phpVideoPro
+# ========================================================
+
+INSERT INTO lang (message_id,lang,content) VALUES ('not_yet_implemented','de','Sorry - leider noch nicht verfügbar.');
+
+# when finished, activate language
+UPDATE languages SET available='Yes' WHERE lang_id='de';

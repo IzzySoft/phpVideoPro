3f8e8c940842671fea031fe1b20ad3a2e6545dba
Izzy <izzysoft@qumran.org>
2001-08-09 07:20:01 +0000

    * prepared for update to 0.2.0 (removed some code not needed anymore, plus added other code instead :)
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@138 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

---
+++ setup/0-1-5_to_0-2-0.sql
@@ -0,0 +1,6 @@
+# ========================================================
+# Updating Database for phpVideoPro from v0.1.5 to v0.2.0
+# ========================================================
+
+# version update
+UPDATE pvp_config SET value='0.2.0' WHERE name='version';

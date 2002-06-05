9c96e145dc50e7c56167a2ca592f884b44f656aa
Izzy <izzysoft@qumran.org>
2002-06-05 13:41:17 +0000

    + added update sequence from v0.2.2 to v0.2.3
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@291 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

--- setup/update.php
+++ setup/update.php
@@ -95,8 +95,8 @@
       case "0.1.6"    : queryf("0-1-6_to_0-1-7.sql","Update from v0.1.6 to v0.1.7");
       case "0.1.7"    : queryf("0-1-7_to_0-2-0.sql","Update from v0.1.7 to v0.2.0");
       case "0.2.0"    : queryf("0-2-0_to_0-2-1." . $database["type"],"Update from v0.2.0 to v0.2.1");
-                        break;
       case "0.2.1"    : queryf("0-2-1_to_0-2-2." . $database["type"],"Update from v0.2.1 to v0.2.2");
+      case "0.2.2"    : queryf("0-2-2_to_0-2-3.sql","Update from v0.2.2 to v0.2.3");
                         break;
       default         : $final = "Your database version seems to be current, there's nothing I can update for you!";
     }

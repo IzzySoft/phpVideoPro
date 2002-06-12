797f0da9b9194e1d97e2103dc502fd839da36a93
Izzy <izzysoft@qumran.org>
2002-06-12 09:46:56 +0000

    initial check-in of label printer
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@308 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

---
+++ label.php
@@ -0,0 +1,37 @@
+<?php
+ /***************************************************************************\
+ * phpVideoPro                                   (c) 2001 by Itzchak Rehberg *
+ * written by Itzchak Rehberg <izzysoft@qumran.org>                          *
+ * http://www.qumran.org/homes/izzy/                                         *
+ * --------------------------------------------------------------------------*
+ * This program is free software; you can redistribute and/or modify it      *
+ * under the terms of the GNU General Public License (see doc/LICENSE)       *
+ \***************************************************************************/
+
+ /* $Id$ */
+
+ $silent = TRUE;
+ include("inc/header.inc");
+ include("inc/label.inc");
+ 
+ $query = "SELECT title,length FROM video"
+        . " WHERE cass_id=$cass_id AND mtype_id=$mtype_id";
+ $db->dbquery($query);
+ $i = 0;
+ while ( $db->next_record() ) {
+   $title  = $db->f('title');
+   $len = $db->f('length');
+   $len_min = $len % 60; if ($len_min < 10) $len_min = "0" . $len_min;
+   $length = floor($len / 60) . ":" . $len_min;
+   $text[] = "$title   $length";
+   ++$i;
+ }
+
+ $label  = new label("default");
+ $label->write($cass_id,$text);
+ $label->prn();
+ $label->destroy();
+ 
+ include("inc/footer.inc");
+
+?>

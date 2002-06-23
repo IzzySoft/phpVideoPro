78d813e380724273bed8de5f5f8490a817bfa05a
Izzy <izzysoft@qumran.org>
2002-06-23 18:31:47 +0000

    + outsourced the real list creation and added list by category
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@390 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

--- listgen.php
+++ listgen.php
@@ -32,11 +32,10 @@
 
    // obtain the list of movies and prepare some default settings
    $pagewidth  = 98;
-   $movielist = $db->get_movielist($order);
-   $listlength = count($movielist);
    switch ($order) {
-     case "title" : $listtitle = lang("medialist_alpha"); break;
-     default      : $listtitle = lang("medialist_num"); break;
+     case "title" : $listtitle = lang("medialist_alpha"); $pfile="movie"; break;
+     case "cat"   : $listtitle = lang("catlist_alpha"); $pfile="cat"; break;
+     default      : $listtitle = lang("medialist_num"); $pfile="movie"; break;
    }
    $listtitle .= " (" . $pvp->common->formatDate(date('Y'),date('m'),date('d')) . ")";
    switch ($outputtype) {
@@ -44,54 +43,9 @@
      default    : $sep = " "; $align = TRUE; $multipage = TRUE; $ext = "txt"; break;
    }
    if ($align) $listtitle = $pvp->common->centerstr($listtitle,$pagewidth);
-   if ($align) {
-     $listheader = $pvp->common->centerstr(lang("medium"),12) . $sep
-                 . $pvp->common->centerstr(lang("title"),55) . $sep
-                 . $pvp->common->centerstr(lang("category"),17) . $sep
-                 . $pvp->common->centerstr(lang("length"),5) . $sep
-	         . $pvp->common->centerstr("LP",4);
-   } else {
-     $listheader = lang("medium") . $sep
-                 . lang("title") . $sep
-                 . lang("category") . $sep
-                 . lang("length") . $sep
-	         . "LP";
-   }
-   for ($i=0;$i<$pagewidth;$i++) {
-     $underline .= "-";
-   }
-   $pagelength -= 4; // we need 4 lines for the title & header
-   $pages = ceil($listlength / $pagelength); $page = 0;
 
-   // build the list
-   if (!$multipage) $out = "$listheader\n";
-   for ($i=0;$i<$listlength;$i++) {
-     if ($multipage && !($i % $pagelength)) { // new page
-       ++$page;
-       if ($i) $out .= "\x0C"; // no formfeed on the first page
-       $out .= listtitle($page,$pages) . "\n";
-       $out .= "\n$listheader\n$underline\n";
-     }
-     if (strlen($movielist[$i][lp])) { $lp = lang("yes"); } else { $lp = lang("no"); }
-     if ($align) {
-       $title  = $pvp->common->fillblanks($movielist[$i][title],55);
-       $cat    = $pvp->common->centerstr($movielist[$i][cat1],17);
-       $length = $pvp->common->padblanks($movielist[$i][length],5);
-       $lp     = $pvp->common->centerstr($lp,4);
-     } else {
-       $title  = $movielist[$i][title];
-       $cat    = $movielist[$i][cat1];
-       $length = $movielist[$i][length];
-     }
-     $line = $movielist[$i][mtype_short] . " "
-           . $pvp->common->padzeros($movielist[$i][cass_id],4) . "-"
-           . $pvp->common->padzeros($movielist[$i][part],2) . $sep
-	   . $title . $sep
-	   . $cat . $sep
-	   . $length . $sep
-	   . $lp;
-     $out .= "$line\n";
-   }
+   include("inc/listgen_$pfile.inc");
+
    if ($multipage && (--$i % $pagelength)) $out .= "\x0C"; // formfeed after last page if not done
 
    // send it to the browser
@@ -108,7 +62,8 @@
  $t->set_block("list","definitionblock","definitionlist");
  $list   = "<SELECT NAME=\"order\">"
          . "<OPTION VALUE=\"num\">" . lang("medialist_num"). "</OPTION>"
-	 . "<OPTION VALUE=\"title\">" . lang("medialist_alpha") . "</OPTION></SELECT>";
+	 . "<OPTION VALUE=\"title\">" . lang("medialist_alpha") . "</OPTION>"
+	 . "<OPTION VALUE=\"cat\">" . lang("catlist_alpha") . "</OPTION></SELECT>";
  $format = "<SELECT NAME=\"outputtype\"><OPTION VALUE=\"ascii\">ASCII</OPTION>"
          . "<OPTION VALUE=\"csv\">CSV</OPTION></SELECT>";
  $lines  = "<INPUT NAME=\"pagelength\" VALUE=\"$pagelength\" SIZE=\"3\">";

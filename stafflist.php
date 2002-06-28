7a728eb493b1743fcaf3fc535df22e64f4aa5bb0
Izzy <izzysoft@qumran.org>
2002-06-28 11:04:33 +0000

    * replaced some db dependent stuff
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@424 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

--- stafflist.php
+++ stafflist.php
@@ -22,80 +22,46 @@
   if (!$start) $start = 0;
   include("inc/nextmatch.inc");
 
-  function getStaffClause($i) {
-    GLOBAL $staff,$stafftype;
-    switch ($stafftype) {
-      case "actors"     : $staff_clause = " (v.actor1_id=" . $staff[$i]->id
-                                        . " AND v.actor1_list=1) OR"
-					. " (v.actor2_id=" . $staff[$i]->id
-                                        . " AND v.actor2_list=1) OR"
-					. " (v.actor3_id=" . $staff[$i]->id
-                                        . " AND v.actor3_list=1) OR"
-					. " (v.actor4_id=" . $staff[$i]->id
-                                        . " AND v.actor4_list=1) OR"
-					. " (v.actor5_id=" . $staff[$i]->id
-                                        . " AND v.actor5_list=1))";
-					break;
-      case "directors" : $staff_clause = " (v.director_id=" . $staff[$i]->id
-                                       . " AND v.director_list=1))";
-                         break;
-      case "music"     : $staff_clause = " (v.music_id=" . $staff[$i]->id
-                                       . " AND v.music_list=1))";
-                         break;
-      default          : break;
-    }
-    return $staff_clause;
-  }
-  
   // retrieve all staff members
-#  $query="SELECT id,name,firstname FROM $stafftype ORDER BY id";
-  $query="SELECT id,name,firstname FROM $stafftype ORDER BY name,firstname";
+  $query="SELECT id FROM $stafftype ORDER BY name,firstname";
   $nextmatch = new nextmatch ($query,$pvp->tpl_dir,$PHP_SELF."?stafftype=$stafftype",$start);
   $i = 0;
   while ( $db->next_record() ) {
-    $staff[$i]->id        = $db->f('id');
-    $staff[$i]->name      = $db->f('name');
-    $staff[$i]->firstname = $db->f('firstname');
+    $staff[$i][id]        = $db->f('id');
     $i++;
+  }
+  for ($k=0;$k<$i;$k++) {
+    switch ($stafftype) {
+      case "actors"    : $staff[$k][name] = $db->get_actor($staff[$k][id]); break;
+      case "directors" : $staff[$k][name] = $db->get_director($staff[$k][id]); break;
+      case "music"     : $staff[$k][name] = $db->get_music($staff[$k][id]); break;
+    }
   }
 
   // now get & draw the list
   $row = 0;
   for ($i=0;$i<count($staff);$i++) {
-    $query  = "SELECT v.cass_id,v.part,v.title,v.length,v.year,v.aq_date,c.name,m.sname,v.mtype_id,";
-    switch ($stafftype) {
-      case "actors"   : $query .= "v.actor1_id,v.actor2_id,v.actor3_id,v.actor4_id,v.actor5_id,"
-                                . "v.actor1_list,v.actor2_list,v.actor3_list,v.actor4_list,v.actor5_list";
-			break;
-      case "directors": $query .= "v.director_id,v.director_list"; break;
-      case "music"    : $query .= "v.music_id,v.music_list"; break;
-      default         : break;
-    }
-    $query .= " FROM video v, cat c, mtypes m";
-    $query .= " WHERE v.cat1_id=c.id AND v.mtype_id=m.id AND ("
-            . getStaffClause($i);
-    if ( strlen($filter) ) $query .= " AND ($filter)";
+    $movies = $db->get_movienamelist($stafftype,$staff[$i][name],$filter);
+    $moviecount = count($movies);
     $same_name = FALSE;
-    dbquery($query);
-    $same_name = FALSE;
-    while ($db->next_record()) {
+    for ($k=0;$k<$moviecount;$k++) {
      $row++;
-     $mtype    = $db->f('sname');
-     $mtype_id = $db->f('mtype_id');
-     $cass_id  = $db->f('cass_id');
+     $mtype    = $movies[$k][mtype_short];
+     $mtype_id = $movies[$k][mtype_id];
+     $cass_id  = $movies[$k][cass_id];
      $nr       = $cass_id;
-     $part     = $db->f('part');
+     $part     = $movies[$k][part];
      while (strlen($nr)<4) { $nr = "0" . $nr; }
      $nr      .= "-";
      if (strlen($part)<2) {
        $nr .= "0" . $part;
      } else { $nr .= $part; }
      $movie_id = urlencode($mtype . " " . $nr);
-     $title    = $db->f('title'); check_empty($title);
-     $length   = $db->f('length'); check_empty($length);
-     $year     = $db->f('year'); check_empty($year);
-     $aq_date  = $db->f('aq_date');  check_empty($aq_date);
-     $category = $db->f('name'); check_empty($category);
+     $title    = $movies[$k][title]; check_empty($title);
+     $length   = $movies[$k][length]; check_empty($length);
+     $year     = $movies[$k][year]; check_empty($year);
+     $aq_date  = $movies[$k][aq_date];  check_empty($aq_date);
+     $category = $movies[$k][cat1]; check_empty($category);
      debug("D","Got '$nr' ($title), RowCount now: '$row'.<br>");
 
      if ($same_name) {
@@ -103,9 +69,9 @@
        $t->set_var("namesep","&nbsp;");
        $t->set_var("firstname","&nbsp;");
      } else {
-       $t->set_var("name",$staff[$i]->name);
+       $t->set_var("name",$staff[$i][name][name]);
        $t->set_var("namesep",", ");
-       $t->set_var("firstname",$staff[$i]->firstname);
+       $t->set_var("firstname",$staff[$i][name][firstname]);
      }
      $t->set_var("title",$title);
      $t->set_var("category",$category);

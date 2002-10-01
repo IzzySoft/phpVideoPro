a0b4dab0ed79d6378f5c3e3d9bfbbcd6479eb755
Izzy <izzysoft@qumran.org>
2002-10-01 22:16:36 +0000

    + language refresh added to update for v0.3.1
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@544 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

--- setup/0-2-8_to_0-3-1.sql
+++ setup/0-2-8_to_0-3-1.sql
@@ -4,3 +4,6 @@
 
 # version update
 UPDATE pvp_config SET value='0.3.1' WHERE name='version';
+
+# prepare default language update
+DELETE FROM lang WHERE lang='en';
--- setup/update.php
+++ setup/update.php
@@ -100,10 +100,10 @@
       case "0.2.5"    : queryf("0-2-5_to_0-2-6.sql","Update from v0.2.5 to v0.2.6");
       case "0.2.6"    : queryf("0-2-6_to_0-2-7." . $database["type"],"Update from v0.2.6 to v0.2.7");
       case "0.2.7"    : queryf("0-2-7_to_0-2-8.sql","Update from v0.2.7 to v0.2.8");
-                        queryf("lang_en.sql","Refresh of English language support");
                         queryf("categories.sql","Refresh of categories");
       case "0.2.8"    :
       case "0.3.0"    : queryf("0-2-8_to_0-3-1.sql","Update from v0.2.8/9 to v0.3.1");
+                        queryf("lang_en.sql","Refresh of English language support");
                         break;
       default         : $final = "Your database version seems to be current, there's nothing I can update for you!";
     }

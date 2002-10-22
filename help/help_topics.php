e45f16cd796883de9241c7a7b8a32fe12c5e8373
Izzy <izzysoft@qumran.org>
2002-10-22 21:16:40 +0000

    + added missing items to content list
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@657 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

--- help/help_topics.php
+++ help/help_topics.php
@@ -1,14 +1,14 @@
 <?php
- /***************************************************************************\
- * phpVideoPro                                   (c) 2001 by Itzchak Rehberg *
- * written by Itzchak Rehberg <izzysoft@qumran.org>                          *
- * http://www.qumran.org/homes/izzy/                                         *
- * --------------------------------------------------------------------------*
- * This program is free software; you can redistribute and/or modify it      *
- * under the terms of the GNU General Public License (see doc/LICENSE)       *
- * --------------------------------------------------------------------------*
- * Help System: Create the Help Index                                        *
- \***************************************************************************/
+ #############################################################################
+ # phpVideoPro                                   (c) 2001 by Itzchak Rehberg #
+ # written by Itzchak Rehberg <izzysoft@qumran.org>                          #
+ # http://www.qumran.org/homes/izzy/                                         #
+ # ------------------------------------------------------------------------- #
+ # This program is free software; you can redistribute and/or modify it      
+ # under the terms of the GNU General Public License (see doc/LICENSE)       #
+ # ------------------------------------------------------------------------- #
+ # Help System: Create the Help Index                                        #
+ #############################################################################
 
  /* $Id$ */
 
@@ -19,6 +19,7 @@
    li("edit");
     li("add_entry",2);
     li("configuration",2);
+    li("admin_cats",2);
    li("filter");
     li("set_filter",2);
     li("unset_filter",2);
@@ -35,6 +36,7 @@
     li("listgen",2);
   headline("other_pages");
    li("view_entry");
+   li("media_change");
   headline("howto");
    li("howto_lang");
    li("howto_help");

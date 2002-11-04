7cb16d5c49848a2149b0ef9bc8151addb44fc94a
Izzy <izzysoft@qumran.org>
2002-11-04 22:22:17 +0000

    ! fixed misspelled table name
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@765 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

--- setup/0-3-6_to_0-3-7.sql
+++ setup/0-3-6_to_0-3-7.sql
@@ -5,7 +5,7 @@
 # moved items from config.inc to DB
 INSERT INTO pvp_config (name,value) VALUES ("rw_media","1");
 INSERT INTO pvp_config (name,value) VALUES ("remove_empty_media","1");
-INSERT INTO pvp_preferences (name,value) VALUES ("default_movie_colorid","2");
+INSERT INTO preferences (name,value) VALUES ("default_movie_colorid","2");
 
 # version update
 UPDATE pvp_config SET value='0.3.6' WHERE name='version';

42ab013945eec446dd60be4d25e1e940841008cc
Izzy <izzysoft@qumran.org>
2002-11-21 22:22:11 +0000

    initial check-in
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@827 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

---
+++ admin/users.php
@@ -0,0 +1,84 @@
+<?php
+ #############################################################################
+ # phpVideoPro                                   (c) 2001 by Itzchak Rehberg #
+ # written by Itzchak Rehberg <izzysoft@qumran.org>                          #
+ # http://www.qumran.org/homes/izzy/                                         #
+ # ------------------------------------------------------------------------- #
+ # This program is free software; you can redistribute and/or modify it      #
+ # under the terms of the GNU General Public License (see doc/LICENSE)       #
+ # ------------------------------------------------------------------------- #
+ # Administration: User Access Management                                    #
+ #############################################################################
+
+ /* $Id$ */
+
+ $page_id = "admin_users";
+ include("../inc/includes.inc");
+
+ #==================================================[ process the changes ]===
+ if ($update) {
+   for ($i=0;$i<$lines;++$i) {
+     $user->id     = ${"user_".$i};
+     $user->login  = ${"user_".$i."_login"};
+     $user->comment= ${"user_".$i."_comment"};
+     $access = array("admin","browse","add","upd","del");
+     foreach ($access as $value) {
+       $var = $value ."_".$user->id;
+       if (${$var}) { $user->$value = "1"; } else { $user->$value = "0"; }
+     }
+     if ( !$db->set_user($user) ) $error .= ",".$user->id;
+   }
+   if ($error) {
+     $error = substr($error,1);
+     $save_result = $colors["err"] . lang("user_update_failed",$error) . "</Font><BR>\n";
+   } else {
+     $save_result = $colors["ok"] . lang("update_success") . ".</Font><BR>\n";
+   }
+ }
+
+ #=======================================================[ build the form ]===
+ $t = new Template($pvp->tpl_dir);
+ $t->set_file(array("template"=>"admin_users.tpl"));
+ $t->set_block("template","itemblock","item");
+
+ $users = $db->get_userlist();
+ $usercount = count($users);
+ for ($i=0;$i<$usercount;++$i) {
+   $t->set_var("user_id","<INPUT TYPE='hidden' NAME='user_".$i."' VALUE='".$users[$i]->id."'>".$users[$i]->id);
+   $t->set_var("login","<INPUT TYPE='hidden' NAME='user_".$i."_login' VALUE='".$users[$i]->login."'>".$users[$i]->login);
+   $t->set_var("comment","<INPUT TYPE='hidden' NAME='user_".$i."_comment' VALUE='".$users[$i]->comment."'>".$users[$i]->comment);
+   $t->set_var("browse",$pvp->common->make_checkbox("browse_".$users[$i]->id,$users[$i]->browse));
+   $t->set_var("add",$pvp->common->make_checkbox("add_".$users[$i]->id,$users[$i]->add));
+   $t->set_var("upd",$pvp->common->make_checkbox("upd_".$users[$i]->id,$users[$i]->upd));
+   $t->set_var("del",$pvp->common->make_checkbox("del_".$users[$i]->id,$users[$i]->del));
+   $t->set_var("isadmin",$pvp->common->make_checkbox("admin_".$users[$i]->id,$users[$i]->admin));
+   $t->set_var("edit","<A HREF='useredit.php?id=".$users[$i]->id."'>".lang("edit")."</A>");
+   $t->set_var("delete","<A HREF='useredit.php?delete=".$users[$i]->id."'>".lang("delete")."</A>");
+   $t->parse("item","itemblock",TRUE);
+ }
+
+ $t->set_var("listtitle",lang("admin_users"));
+ $t->set_var("formtarget",$PHP_SELF);
+ $t->set_var("update","<INPUT TYPE='submit' NAME='update' VALUE=".lang("update").">");
+ $t->set_var("adduser","<A HREF='useredit.php?addnew=1'>".lang("add_user")."</A>");
+ $t->set_var("save_result",$save_result);
+ $t->set_var("head_users",lang("user"));
+ $t->set_var("head_access",lang("data_access"));
+ $t->set_var("head_admin",lang("admin"));
+ $t->set_var("head_actions",lang("actions"));
+ $t->set_var("head_id","ID");
+ $t->set_var("head_login",lang("login"));
+ $t->set_var("head_comment",lang("comments"));
+ $t->set_var("head_browse",lang("read_access_short"));
+ $t->set_var("head_add",lang("add_access_short"));
+ $t->set_var("head_upd",lang("upd_access_short"));
+ $t->set_var("head_del",lang("del_access_short"));
+ $t->set_var("head_isadmin",lang("admin_access_short"));
+ $t->set_var("head_edit",lang("edit"));
+ $t->set_var("head_delete",lang("delete"));
+ $t->set_var("lines",$usercount);
+ include("../inc/header.inc");
+ $t->pparse("out","template");
+
+ include("../inc/footer.inc");
+?>
---
+++ templates/default/admin_users.tpl
@@ -0,0 +1,40 @@
+<H2 ALIGN="center">{listtitle}</H2>
+<CENTER>{save_result}</CENTER>
+<FORM NAME="admin_users" METHOD="post" ACTION="{formtarget}">
+<TABLE ALIGN="center" BORDER="0"><TR><TD>
+<TABLE ALIGN="center" BORDER="1">
+ <TR><TH COLSPAN="3"><DIV ALIGN="center">{head_users}</DIV></TH>
+     <TH COLSPAN="4"><DIV ALIGN="center">{head_access}</DIV></TH>
+     <TH><DIV ALIGN="center">{head_admin}</DIV></TH>
+     <TH COLSPAN="2"><DIV ALIGN="center">{head_actions}</DIV></TH></TR>
+ <TR><TH><DIV ALIGN="center">{head_id}</DIV></TH>
+     <TH><DIV ALIGN="center">{head_login}</DIV></TH>
+     <TH><DIV ALIGN="center">{head_comment}</DIV></TH>
+     <TH><DIV ALIGN="center">{head_browse}</DIV></TH>
+     <TH><DIV ALIGN="center">{head_add}</DIV></TH>
+     <TH><DIV ALIGN="center">{head_upd}</DIV></TH>
+     <TH><DIV ALIGN="center">{head_del}</DIV></TH>
+     <TH><DIV ALIGN="center">{head_isadmin}</DIV></TH>
+     <TH><DIV ALIGN="center">{head_edit}</DIV></TH>
+     <TH><DIV ALIGN="center">{head_delete}</DIV></TH></TR>
+<!-- BEGIN itemblock -->
+ <TR><TD>{user_id}</TD>
+     <TD>{login}</TD>
+     <TD>{comment}</TD>
+     <TD><DIV ALIGN="center">{browse}</DIV></TD>
+     <TD><DIV ALIGN="center">{add}</DIV></TD>
+     <TD><DIV ALIGN="center">{upd}</DIV></TD>
+     <TD><DIV ALIGN="center">{del}</DIV></TD>
+     <TD><DIV ALIGN="center">{isadmin}</DIV></TD>
+     <TD><DIV ALIGN="center">{edit}</DIV></TD>
+     <TD><DIV ALIGN="center">{delete}</DIV></TD>
+     </TR>
+<!-- END itemblock -->
+</TABLE></TD></TR><TR><TD>
+<TABLE WIDTH="100%" BORDER="0">
+ <TR><TH><DIV ALIGN="center">{update}</DIV></TH>
+     <TH><DIV ALIGN="center">{adduser}</DIV></TH></TR>
+</TABLE></TD></TR>
+<INPUT TYPE='hidden' NAME='lines' VALUE='{lines}'>
+</TABLE>
+</FORM>

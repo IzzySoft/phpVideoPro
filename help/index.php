5e18606b80aef815b3f2650620596d4b58142ab4
Izzy <izzysoft@qumran.org>
2003-04-15 21:36:43 +0000

    + added nav to switch to English help files
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@1034 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

--- help/index.php
+++ help/index.php
@@ -28,7 +28,11 @@ include ("../inc/class.template.inc");
 function helppage ($topic) {
   GLOBAL $pvp,$PHP_SELF;
   $desc = lang($topic);
-  $lang = $pvp->preferences->get("lang");
+  if ( $GLOBALS["force_en"] ) {
+    $lang = "en";
+  } else {
+    $lang = $pvp->preferences->get("lang");
+  }
   $name = $topic . ".inc";
   $file = dirname(__FILE__) . "/" . $lang . "/" . $name;
   $default_file = dirname(__FILE__) . "/en/" . $name;
@@ -290,6 +294,8 @@ echo "</HEAD><BODY>\n";
 
 $pm = new pagemaker;
 $pm->set_nav("back","<A HREF=\"JavaScript:history.back()\">" . lang("back") . "</A>");
+if ( !$force_en && $pvp->preferences->get("lang")!="en" )
+  $pm->set_nav("force","<A HREF='$PHP_SELF?topic=$topic&force_en=1'><IMG SRC='$base_url/templates/default/images/english.jpg'></A>");
 $pm->set_nav("index","<A HREF=\"$PHP_SELF\">" . lang("index") . "</A>");
 $pm->set_nav("close","<A HREF=\"JavaScript:window.close()\">" . lang("close") . "</A>");
 if ($topic) { // display specific help page

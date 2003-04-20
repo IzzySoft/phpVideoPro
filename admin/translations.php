88466536dfb31cfd01a8e421a9c22bb46c672101
Izzy <izzysoft@qumran.org>
2003-04-20 16:09:03 +0000

    initial check-in
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@1043 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

---
+++ admin/translations.php
@@ -0,0 +1,88 @@
+<?php
+ #############################################################################
+ # phpVideoPro                              (c) 2001-2003 by Itzchak Rehberg #
+ # written by Itzchak Rehberg <izzysoft@qumran.org>                          #
+ # http://www.qumran.org/homes/izzy/                                         #
+ # ------------------------------------------------------------------------- #
+ # This program is free software; you can redistribute and/or modify it      #
+ # under the terms of the GNU General Public License (see doc/LICENSE)       #
+ # ------------------------------------------------------------------------- #
+ # Edit Translations                                                         #
+ #############################################################################
+
+ /* $Id$ */
+
+ #========================================================[ initial setup ]==
+ $page_id = "admin_translations";
+ include("../inc/includes.inc");
+ if (!$pvp->auth->browse) kickoff();
+ $filter = get_filters();
+ if (!$start) $start = 0;
+# $targetlang = "de";
+ include("../inc/class.nextmatch.inc");
+
+ $t = new Template($pvp->tpl_dir);
+ $t->set_file(array("list"=>"admin_translations.tpl"));
+ $t->set_block("list","mtitleblock","mtitlelist");
+ $t->set_block("list","mdatablock","mdatalist");
+ $t->set_block("list","emptyblock","emptylist");
+
+ #======================================================[ init target lang ]==
+ if (!$targetlang) {
+   $sel_lang = "<SELECT NAME='targetlang'>";
+   $langs    = $db->get_languages();
+   $lcount   = count($langs);
+   for ($i=0;$i<$lcount;++$i) {
+     $sel_lang .= "<OPTION VALUE='".$langs[$i]["id"]."'>".$langs[$i]["name"]."</OPTION>";
+   }
+   $sel_lang .= "</SELECT>";
+   $t->set_var("sel_lang_title",lang("sel_target_lang"));
+   $t->set_var("sel_lang",$sel_lang);
+   $t->parse("emptylist","emptyblock");
+   $t->set_var("submit",lang("submit"));
+   include("../inc/header.inc");
+   $t->pparse("out","list");
+   include("../inc/footer.inc");
+   exit;
+ }
+
+
+ #=======================================[ get movies and setup variables ]===
+ $query = "\$db->get_singletrans(\"en\",$start)";
+ $nextmatch = new nextmatch ($query,$pvp->tpl_dir,$PHP_SELF."?targetlang=$targetlang",$start);
+
+ $list = $nextmatch->list;
+ for ($i=0;$i<$nextmatch->listcount -1;$i++) {
+   $msgid  = $list["xlist"][$i];
+   if ($submit) $db->set_translation($msgid,${$msgid."_trans"},$targetlang);
+   $orig   = $list["$msgid"];
+   $target = $db->get_singletrans($targetlang,"",$msgid);
+   if ($target[$msgid]) $target[$msgid] = htmlentities($target[$msgid]);
+   if ($orig) $orig = htmlentities($orig);
+   $t->set_var("code",$msgid);
+   $t->set_var("orig",$orig);
+   $rows  = ceil( strlen($orig)/40 ) +1;
+   $input = "<TEXTAREA NAME='$msgid"."_trans' COLS='50' ROWS='$rows'>".$target[$msgid]."</TEXTAREA>";
+   $t->set_var("trans",$input);
+   $t->set_var("sample","");
+   $t->parse("mdatalist","mdatablock",TRUE);
+ }
+ $hidden = "<INPUT TYPE='hidden' NAME='targetlang' VALUE='$targetlang'>";
+ if ($start) $hidden .= "<INPUT TYPE='hidden' NAME='start' VALUE='$start'>";
+ $t->set_var("code",lang("trans_code"));
+ $t->set_var("orig",lang("orig_trans"));
+ $t->set_var("trans",lang("target_trans"));
+ $t->set_var("sample",lang("trans_sample"));
+ $t->set_var("submit",lang("update"));
+ $t->set_var("hidden",$hidden);
+ $t->set_var("first",$nextmatch->first);
+ $t->set_var("left",$nextmatch->left);
+ $t->set_var("right",$nextmatch->right);
+ $t->set_var("last",$nextmatch->last);
+ $t->parse("mtitlelist","mtitleblock");
+
+ include("../inc/header.inc");
+ $t->pparse("out","list");
+ include("../inc/footer.inc");
+
+?>
---
+++ templates/default/admin_translations.tpl
@@ -0,0 +1,33 @@
+<H2 Align=Center>{listtitle}</H2>
+<TABLE ALIGN="center" BORDER="0" WIDTH="95%">
+ <FORM NAME="transform" METHOD="post" ACTION="{formtarget}">
+ {hidden}
+ <TR><TD>{first}{left}</TD>
+     <TD><DIV ALIGN="RIGHT">{right}{last}</DIV></TD></TR>
+ <TR><TD COLSPAN=2>
+  <TABLE ALIGN="center" BORDER="1" CELLPADDING="2" CELLSPACING="2" WIDTH="100%" Style="table-layout:fixed">
+   <!-- BEGIN mtitleblock -->
+   <COLGROUP><COL WIDTH="150 pt"><COL WIDTH="*"><COL WIDTH="380 pt"><COL WIDTH="100 pt"></COLGROUP -->
+   <TR><TH>{code}</TH>
+       <TH>{orig}</TH>
+       <TH>{trans}</A></TH>
+       <TH>{sample}</A></TH>
+   </TR>
+   <!-- END mtitleblock -->
+   <!-- BEGIN mdatablock -->
+   <TR><TD>{code}</TD>
+       <TD>{orig}</TD>
+       <TD>{trans}</TD>
+       <TD>{sample}</TD>
+   </TR>
+   <!-- END mdatablock -->
+   <!-- BEGIN emptyblock -->
+   <TR><TH>{sel_lang_title}</TH><TD>{sel_lang}</TD></TR>
+   <!-- END emptyblock -->
+   <TR><TH COLSPAN="4"><DIV ALIGN="center"><INPUT TYPE="submit" NAME="submit" VALUE="{submit}"></DIV></TH></TR>
+  </Table>
+ </TD></TR>
+ <TR><TD>{first}{left}</TD>
+     <TD><DIV ALIGN="RIGHT">{right}{last}</DIV></TD></TR>
+ </FORM>
+</TABLE>

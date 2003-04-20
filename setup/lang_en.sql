4e4d199643a23c25b5d7b34e030731e97d329b01
Izzy <izzysoft@qumran.org>
2003-04-20 16:06:13 +0000

    .
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@1041 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

--- setup/lang_en.sql
+++ setup/lang_en.sql
@@ -212,7 +212,7 @@ INSERT INTO lang VALUES('stat_categories','en','Categories (used/all)');
 INSERT INTO lang VALUES('stats','en','Statistics');
 INSERT INTO lang VALUES('dl_pvp_latest','en','D/L latest version');
 INSERT INTO lang VALUES('info_update','en','Update Information');
-INSERT INTO lang VALUES('info_noupdate','en','<DIV ALIGN="center">The installed version of phpVideoPro (%1) is recent - there\'s no newer version available for download. To download the tarball of the installed version, please follow <A HREF=http://www.qumran.org/homes/izzy/software/get_update.php?program=pvp" TARGET="_blank">this link</A>.</DIV>');
+INSERT INTO lang VALUES('info_noupdate','en','<DIV ALIGN="center">The installed version of phpVideoPro (%1) is recent - there\'s no newer version available for download. To download the tarball of the installed version, please follow <A HREF="http://www.qumran.org/homes/izzy/software/get_update.php?program=pvp" TARGET="_blank">this link</A>.</DIV>');
 INSERT INTO lang VALUES('info_common','en','Common Information');
 INSERT INTO lang VALUES('info_common_info','en','<DIV ALIGN="center">This is version %1 of phpVideoPro, running on %2.</DIV>');
 INSERT INTO lang VALUES('change_nr','en','Change MediaNr/MediaType for %1');

f49727f7c36d3e02663d0f6cfd9e9cee27f4591f
Izzy <izzysoft@qumran.org>
2003-04-21 16:29:08 +0000

    initial check-in
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@1062 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

---
+++ medium.php
@@ -0,0 +1,66 @@
+<?php
+ #############################################################################
+ # phpVideoPro                               (c) 200-2003 by Itzchak Rehberg #
+ # written by Itzchak Rehberg <izzysoft@qumran.org>                          #
+ # http://www.qumran.org/homes/izzy/                                         #
+ # ------------------------------------------------------------------------- #
+ # This program is free software; you can redistribute and/or modify it      #
+ # under the terms of the GNU General Public License (see doc/LICENSE)       #
+ # ------------------------------------------------------------------------- #
+ # Display data for a given medium                                           #
+ #############################################################################
+
+ /* $Id$ */
+
+# $page_id = "admin_cats";
+ include("inc/includes.inc");
+ $mtype  = $db->get_mtypes("id=$mtype_id");
+ $medium = $mtype[0]["sname"] ." ". $cass_id;
+ $mid    = $db->get_movieid($mtype_id,$cass_id);
+
+ $t = new Template($pvp->tpl_dir);
+ $t->set_file(array("template"=>"medium.tpl"));
+ $t->set_block("template","techblock","tech");
+ $t->set_block("template","movieblock","movie");
+ $t->set_var("listtitle",lang("medium_overview",$medium));
+ $t->set_var("techdata",lang("techdata"));
+ $t->set_var("moviedata",lang("movies"));
+
+ function techbutton ($name,$value) {
+   return "<INPUT TYPE='button' NAME='name' VALUE='$value' CLASS='yesnobutton'>";
+ }
+
+ #=============================================[ obtain media information ]===
+ $type = $db->get_mediaspace($cass_id,$mtype_id,"type");
+ $free = $db->get_mediaspace($cass_id,$mtype_id,"free");
+ if ($pvp->common->medium_is_rw($mtype_id)) { $rw = lang("yes"); }
+   else { $rw = lang("no"); }
+ $t->set_var("tname",lang("medialength").":&nbsp;");
+ $t->set_var("tunit",lang("minute_abbrev"));
+ $t->set_var("tdata",techbutton("type",$type));
+ $t->parse("tech","techblock");
+ $t->set_var("tname",lang("free").":&nbsp;");
+ $t->set_var("tdata",techbutton("free",$free));
+ $t->parse("tech","techblock",TRUE);
+ $t->set_var("tname",lang("is_rw").":&nbsp;");
+ $t->set_var("tunit","&nbsp;");
+ $t->set_var("tdata",techbutton("rw",$rw));
+ $t->parse("tech","techblock",TRUE);
+
+ #=============================================[ obtain movie information ]===
+ $mcount = count($mid);
+ for ($i=0;$i<$mcount;++$i) {
+   $movie = $db->get_movie($mid[$i]);
+   $url   = $base_url ."index.php?sel_entry=1&mtype_id=$mtype_id&cass_id=$cass_id&part=".$movie[part];
+   $mlink = $pvp->link->linkurl($url,$movie[part]."&nbsp;");
+   $mdata = $movie[title]." (".$movie[cat1]. ", ".$movie[length]." ".lang("minute_abbrev").")";
+   $t->set_var("mlink",$mlink);
+   $t->set_var("mdata",$mdata);
+   $t->parse("movie","movieblock",TRUE);
+ }
+
+ include("inc/header.inc");
+ $t->pparse("out","template");
+
+ include("inc/footer.inc");
+?>
---
+++ templates/default/medium.tpl
@@ -0,0 +1,17 @@
+ <H2 ALIGN="center">{listtitle}</H2>
+
+ <FORM NAME="mediaform" METHOD="post" ACTION="{form_target}">
+  <TABLE ALIGN="center" BORDER="1">
+    <TR><TH>{techdata}</TH><TH>{moviedata}</TH></TR>
+    <TR><TD STYLE='vertical-align:middle'><TABLE BORDER="0">
+<!-- BEGIN techblock -->
+        <TR><TD><DIV ALIGN="right">{tname}</DIV></TD>
+            <TD>{tdata}</TD><TD>{tunit}</TD></TR>
+<!-- END techblock -->
+        </TABLE></TD><TD STYLE='vertical-align:middle'><TABLE BORDER="0">
+<!-- BEGIN movieblock -->
+        <TR><TD STYLE='text-align:right'>{mlink}</TD><TD>{mdata}</TD></TR>
+<!-- END movieblock -->
+        </TABLE></TD></TR>
+  </TABLE>
+ </FORM>

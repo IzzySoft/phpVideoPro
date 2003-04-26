6ffecf723834162151ea2c1a38a7c2f17ac975ac
Izzy <izzysoft@qumran.org>
2003-04-26 23:26:43 +0000

    initial check-in
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@1099 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

---
+++ admin/printers.php
@@ -0,0 +1,116 @@
+<?php
+ #############################################################################
+ # phpVideoPro                              (c) 2001-2003 by Itzchak Rehberg #
+ # written by Itzchak Rehberg <izzysoft@qumran.org>                          #
+ # http://www.qumran.org/homes/izzy/                                         #
+ # ------------------------------------------------------------------------- #
+ # This program is free software; you can redistribute and/or modify it      #
+ # under the terms of the GNU General Public License (see doc/LICENSE)       #
+ # ------------------------------------------------------------------------- #
+ # Administration: Edit printers                                             #
+ #############################################################################
+
+ /* $Id$ */
+
+  $page_id = "admin_printers";
+  include( dirname(__FILE__) . "/../inc/includes.inc");
+  if (!$pvp->auth->admin) kickoff();
+
+  #-------------------------------------------------------[ process input ]---
+  if ($submit) {
+    for ($i=0;$i<$lines;++$i) {
+      $print_id = "print".$i."_id"; $print_name = "print".$i."_name";
+      $print_unit = "print".$i."_unit"; $print_top = "print".$i."_top";
+      $print_left = "print".$i."_left";
+      if ( !strlen(trim(${$print_name})) ) {
+	  $db->set_printer(${$print_id});
+      } else {
+        if (!${$print_top})  ${$print_top}  = 0;
+        if (!${$print_left}) ${$print_left} = 0;
+        if (!$db->set_printer(${$print_id},${$print_name},${$print_unit},${$print_top},${$print_left})) {
+          $upd .= ",".${$print_name};
+        }
+      }
+    }
+    if ($upd) {
+      $upd = substr($upd,1);
+      $save_result = $colors["err"].lang("printer_upd_failed",$upd)."</FONT><BR>";
+    } else {
+      $save_result = $colors["ok"].lang("update_success")."</FONT><BR>";
+    }
+    if ( strlen(trim($new_name)) ) {
+      if ( !$db->set_printer("",$new_name,$new_unit,$new_top,$new_left) ) {
+        $save_result .= $colors["err"].lang("printer_add_failed")."</FONT><BR>";
+      } else {
+        $save_result .= $colors["ok"].lang("printer_add_success")."</FONT><BR>";
+      }
+    }
+  }
+
+  #-------------------------------------------------------[ build up page ]---
+  $t = new Template($pvp->tpl_dir);
+  $t->set_file(array("template"=>"admin_printers.tpl"));
+  $t->set_block("template","itemblock","item");
+
+  $t->set_var("listtitle",lang("admin_printers"));
+  $t->set_var("save_result",$save_result);
+  $t->set_var("head_print_id","ID");
+  $t->set_var("head_print_name",lang("printer_name"));
+  $t->set_var("head_print_unit",lang("unit"));
+  $t->set_var("head_print_top",lang("top_offset"));
+  $t->set_var("head_print_left",lang("left_offset"));
+  $t->set_var("update","<INPUT TYPE='submit' NAME='submit' VALUE='".lang("update")."'>");
+
+  function make_input($name,$value,$type="text",$class="yesnobutton") {
+    switch($type) {
+      case "hidden" : $input = "<INPUT TYPE='hidden' NAME='$name' VALUE='$value'>"; break;
+      case "button" : $input = "<INPUT TYPE='button' NAME='$name' VALUE='$value' CLASS='yesnobutton'>"; break;
+      default       : $input = "<INPUT NAME='$name' VALUE='$value' CLASS='$class'>"; break;
+    }
+    return $input;
+  }
+
+  function make_unit($name,$value) {
+    GLOBAL $db;
+    static $units, $ucount;
+    if (!$units) {
+      $units  = $db->get_units();
+      $ucount = count($units);
+    }
+    $select = "<SELECT NAME='$name'>";
+    for ($i=0;$i<$ucount;++$i) {
+      $select .= "<OPTION VALUE='".$units[$i][id]."'";
+      if ($units[$i][id]==$value) $select .= " SELECTED";
+      $select .= ">".$units[$i][unit]."</OPTION>";
+    }
+    $select .= "</SELECT>";
+    return $select;
+  }
+
+  $printers = $db->get_printer();
+  $pcount = count($printers);
+  for ($i=0;$i<$pcount;++$i) {
+    $print_id = "print".$i."_id"; $print_name = "print".$i."_name";
+    $print_unit = "print".$i."_unit"; $print_top = "print".$i."_top";
+    $print_left = "print".$i."_left";
+    $t->set_var("print_id",make_input($print_id,$printers[$i]->id,"button").make_input($print_id,$printers[$i]->id,"hidden"));
+    $t->set_var("print_name",make_input($print_name,$printers[$i]->name,"text","catinput"));
+    $t->set_var("print_unit",make_unit($print_unit,$printers[$i]->unit_id));
+    $t->set_var("print_top",make_input($print_top,$printers[$i]->top_offset));
+    $t->set_var("print_left",make_input($print_left,$printers[$i]->left_offset));
+    $t->parse("item","itemblock",TRUE);
+  }
+  $t->set_var("print_id",make_input("new_id","?","button").make_input("new_id","","hidden"));
+  $t->set_var("print_name",make_input("new_name","","text","catinput"));
+  $t->set_var("print_unit",make_unit("new_unit",""));
+  $t->set_var("print_top",make_input("new_top",""));
+  $t->set_var("print_left",make_input("new_left",""));
+  $t->parse("item","itemblock",TRUE);
+  $t->set_var("lines",$pcount);
+
+  include( dirname(__FILE__) . "/../inc/header.inc");
+  $t->pparse("out","template");
+
+  include( dirname(__FILE__) . "/../inc/footer.inc");
+
+?>
---
+++ templates/default/admin_printers.tpl
@@ -0,0 +1,20 @@
+<H2 ALIGN="center">{listtitle}</H2>
+<CENTER>{save_result}</CENTER>
+<FORM NAME="admin_printers" METHOD="post" ACTION="{formtarget}">
+<TABLE ALIGN="center" BORDER="1">
+ <TR><TH><DIV ALIGN="center">{head_print_id}</DIV></TH>
+     <TH><DIV ALIGN="center">{head_print_name}</DIV></TH>
+     <TH><DIV ALIGN="center">{head_print_unit}</DIV></TH>
+     <TH><DIV ALIGN="center">{head_print_top}</DIV></TH>
+     <TH><DIV ALIGN="center">{head_print_left}</DIV></TH></TR>
+<!-- BEGIN itemblock -->
+ <TR><TD><DIV ALIGN="right">{print_id}</DIV></TD>
+     <TD><DIV ALIGN="center">{print_name}</DIV></TD>
+     <TD><DIV ALIGN="center">{print_unit}</DIV></TD>
+     <TD><DIV ALIGN="center">{print_top}</DIV></TD>
+     <TD><DIV ALIGN="center">{print_left}</DIV></TD></TR>
+<!-- END itemblock -->
+ <TR><TD COLSPAN="5"><INPUT TYPE='hidden' NAME='lines' VALUE='{lines}'></TD></TR>
+ <TR><TH COLSPAN="5"><DIV ALIGN="center">{update}</DIV></TH></TR>
+</TABLE>
+</FORM>

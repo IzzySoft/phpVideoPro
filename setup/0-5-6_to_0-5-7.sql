b56da63f622f93005e80544a0fd1455272c24ba1
Izzy <izzysoft@qumran.org>
2004-01-28 21:10:55 +0000

    initial check-in
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@1345 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

---
+++ setup/0-5-6_to_0-5-7.sql
@@ -0,0 +1,9 @@
+# =======================================================
+# Updating Database for phpVideoPro from v0.5.5 to v0.5.6
+# =======================================================
+
+# prepare default lang update
+DELETE FROM lang WHERE lang='en';
+
+# version update
+UPDATE pvp_config SET value='0.5.7' WHERE name='version';

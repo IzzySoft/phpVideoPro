f42f605920184d7c105825eeb164a54181c8e5a2
Izzy <izzysoft@qumran.org>
2004-05-18 18:42:34 +0000

    - removed obsolete files
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@1513 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

--- setup/0-5-6_to_0-5-7.sql
+++
@@ -1,9 +0,0 @@
-# =======================================================
-# Updating Database for phpVideoPro from v0.5.5 to v0.5.8
-# =======================================================
-
-# prepare default lang update
-DELETE FROM lang WHERE lang='en';
-
-# version update
-UPDATE pvp_config SET value='0.5.8' WHERE name='version';

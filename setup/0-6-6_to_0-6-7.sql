43df36928f78ea0a42bfb94fdeb7d0338555bb18
Izzy <izzysoft@qumran.org>
2004-11-12 13:11:18 +0000

    * releasing v0.6.10
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@1852 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

--- setup/0-6-6_to_0-6-7.sql
+++ setup/0-6-6_to_0-6-7.sql
@@ -1,9 +1,9 @@
-# =======================================================
-# Updating Database for phpVideoPro from v0.6.6 to v0.6.9
-# =======================================================
+# ========================================================
+# Updating Database for phpVideoPro from v0.6.6 to v0.6.10
+# ========================================================
 
 # prepare default lang update
 DELETE FROM lang WHERE lang='en';
 
 # version update
-UPDATE pvp_config SET value='0.6.9' WHERE name='version';
+UPDATE pvp_config SET value='0.6.10' WHERE name='version';
--- setup/update.php
+++ setup/update.php
@@ -148,7 +148,8 @@ $pvp->preferences->admin();
     case "0.6.5"    : queryf("0-6-5_to_0-6-6.".$database["type"],"Upgrade to v0.6.6");
     case "0.6.6"    :
     case "0.6.7"    :
-    case "0.6.8"    : queryf("0-6-6_to_0-6-7.sql","Upgrade to v0.6.9");
+    case "0.6.8"    :
+    case "0.6.9"    : queryf("0-6-6_to_0-6-7.sql","Upgrade to v0.6.10");
                       queryf("lang_en.sql","Refresh of English language support");
                       break;
     default         : $final = "Your database version seems to be current, there's nothing I can update for you!";

7f08194776698043d819b3281fd64e7accb49477
Izzy <izzysoft@qumran.org>
2004-11-21 19:58:28 +0000

    * now using the FAQ class (from iFAQMaker) to parse the help files
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@1907 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

--- help/index.php
+++ help/index.php
@@ -16,10 +16,11 @@
 include ("../inc/config.inc");
 include ("../inc/config_internal.inc");
 include ("../inc/common_funcs.inc");
-include ("../inc/class.template.inc");
+require_once ("../inc/class.faq.inc");
 $translations = $db->get_translations( $pvp->preferences->get("lang") );
 if (isset($_GET["topic"])) $topic    = $_GET["topic"]; else $topic = "";
 if (isset($_GET["force_en"])) $force_en = $_GET["force_en"]; else $force_en = FALSE;
+if ( !ini_get("register_globals") ) $PHP_SELF = $_SERVER["PHP_SELF"];
 
 #=============================================[ Get the needed parameters ]===
 /** Find the input file for the requested help topic
@@ -30,7 +31,7 @@ if (isset($_GET["force_en"])) $force_en = $_GET["force_en"]; else $force_en = FA
     page, where URL is the complete HTML &lt;A HREF...&lt/A&gt; string
  */
 function helppage ($topic) {
-  GLOBAL $pvp,$PHP_SELF;
+  GLOBAL $pvp;
   $desc = lang($topic);
   if ( $GLOBALS["force_en"] ) {
     $lang = "en";
@@ -42,10 +43,10 @@ function helppage ($topic) {
   $default_file = dirname(__FILE__) . "/en/" . $name;
   $help->desc = $desc;
   if ( file_exists($file) ) {
-    $help->url  = "<A HREF=\"$PHP_SELF?topic=$topic\">$desc</A>";
+    $help->url  = "<A HREF='".$_SERVER["PHP_SELF"]."?topic=$topic'>$desc</A>";
     $help->file = $file;
   } elseif ( file_exists($default_file) ) {
-    $help->url  = "<A HREF=\"$PHP_SELF?topic=$topic\">$desc</A>";
+    $help->url  = "<A HREF='".$_SERVER["PHP_SELF"]."?topic=$topic'>$desc</A>";
     $help->file = $default_file;
   } else {
     $help->url  = FALSE;
@@ -105,193 +106,12 @@ function headline($topic,$level=0) {
  * @variable string content
  */
 
-#====================================================[ Build page content ]===
-/** Help page creator
- * @package Help
- * @class pagemaker
- */
-class pagemaker {
 
- VAR $t,       // template class
-     $block;   // active block
-
- /** Constructor: Setting up the templates
-  * @constructor pagemaker
-  */
- function pagemaker() {
-  global $pvp;
-  $this->t = new Template($pvp->tpl_dir);
-  $this->t->set_file(array("main"=>"help.tpl"));
-  $this->t->set_block("main","titleblock","titlelist");
-  $this->t->set_block("titleblock","textblock","textlist");
-  $this->block->name = "";
-  $this->block->content = "";
-  $this->block->append = FALSE;
- }
-
- /** Replacing placeholders for variables and translations within the
-  *  input files current block, then add it to the content to parse into
-  *  the templates
-  * @package Help
-  * @class pagemaker
-  * @method add_block
-  * @param string content content block to parse
-  */
- function add_block($content) {
-  if (!$this->block->name) return;
-  if ( preg_match_all("/\{\S+\}/",$content,$matches) ) { // replace variables
-   for ($i=0;$i<count($matches[0]);$i++) {
-     $var = substr($matches[0][$i],1,strlen($matches[0][$i])-2);
-     $pos = strpos($var,"->");
-     if ($pos) {
-      $obj = substr($var,0,$pos); $prop = substr($var,$pos+2);
-      GLOBAL $$obj;
-      $rvar = $$obj->$prop;
-     } else { $rvar = $GLOBALS[$var]; }
-     $content = preg_replace("/\{$var\}/",$rvar,$content);
-   }
-  }
- if ( preg_match_all("/\*\S+\#/",$content,$matches) ) { // replace translations
-   for ($i=0;$i<count($matches[0]);$i++) {
-     $var  = substr($matches[0][$i],1,strlen($matches[0][$i])-2);
-     $rvar = lang($var);
-     $content = preg_replace("/\*$var\#/",$rvar,$content);
-   }
-  }
-  $this->block->content .= $content;
- }
-
- /** Parse the current block into the template
-  * @package Help
-  * @class pagemaker
-  * @method parse_block
-  */
- function parse_block($keep=1) {
-  if (!$this->block->name) return;
-  $list   = $this->block->name . "list";
-  $pblock = $this->block->name . "block";
-  $this->t->set_var($this->block->name,trim($this->block->content));
-  $this->t->parse($list,$pblock,$this->block->append);
-  if (!$this->block->append) $this->block->append = 1;
-  $this->block->content = "";
- }
-
- /** Close the current block. This means, next block won't be appended, but
-  *  a new block will be started
-  * @package Help
-  * @class pagemaker
-  * @method close_block
-  */
- function close_block () {
-  if (!$this->block->name) return;
-  $this->parse_block();
-  $this->block->append  = FALSE;
- }
-
- /** Set template variables. Wrapper around pagemaker::t::set_var
-  * @package Help
-  * @class pagemaker
-  * @method set_nav
-  * @param string name name of template variable
-  * @param string content value of template variable
-  */
- function set_nav($name,$content) {
-   $this->t->set_var($name,$content);
- }
-
- /** Main method - and the only one that should be called from the public.
-  *  This will build the page from the given file, using the given title
-  *  for the headers, and send it to the browser
-  * @package Help
-  * @class pagemaker
-  * @method make_page
-  * @param string title page title
-  * @param string file input file
-  */
- function make_page($title,$file) {
-  $input = file($file);
-  if (substr(trim($input[0]),0,1)=="!") { // "symbolic link"
-    $file = dirname($file) . "/" . trim(substr($input[0],1));
-    $input = file($file);
-  }
-  $line  = 0;
-  $eof = FALSE;
-  while ( $line<count($input) ) {
-   switch ( trim(strtolower($input[$line])) ) {
-    case "[title]"   : if ($this->block->name) {
-                        if ($this->block->name=="title") {
-                         $this->close_block();
-                        } else {
-			 $this->close_block();
-                         $this->block->name = "title";
-                         $this->add_block($this->block->title);
-			 $this->block->append  = TRUE;
-			 $this->close_block();
-			 $this->block->content = "";
-			}
-		       }
-                       $this->block->name = "title";
-		       ++$line;
-                       continue;
-		       break;
-    case "[text]"    : if ($this->block->name) {
-                        if ($this->block->name=="text") {
-                         $this->parse_block();
-                        } else {
-                         $this->block->title   = $this->block->content;
-			 $this->block->content = "";
-                        }
-		       }
-                       $this->block->name = "text";
-		       ++$line;
-                       continue;
-		       break;
-    case "[eof]"     : $eof = TRUE; break;
-    default          : break;
-   }
-   if ($eof) {
-    $this->close_block();
-    $this->block->name = "title";
-    $this->add_block($this->block->title);
-    $this->block->append  = TRUE;
-    $this->close_block();
-    break;
-   }
-   if ( substr(trim($input[$line]),0,1) == "#" ) { // comment line
-     $line++;
-     continue;
-   }
-   switch ($this->block->name) {
-    case "title"   : $this->block->content .= $input[$line]; break;
-    default        : //$this->block->title  = "";
-                     $this->add_block($input[$line]);
-		     break;
-   }
-   $line++;
-  }
-  $this->t->set_var("listtitle",$title);
-  $this->t->pparse("out","main");
- } // end function make_page
-
- /** Template Class to built up pages
-  * @package Help
-  * @class pagemaker
-  * @attribute object t
-  */
- /** Active block we work on
-  * @package Help
-  * @class pagemaker
-  * @attribute object block
-  */
-
-} // end class pagemaker
-
-
-###################==================================[ MAIN - HERE WE GO! ]===
+#====================================================[ MAIN - HERE WE GO! ]===
 $title = "phpVideoPro v$version @ $site - " . lang("help") . ": ";
 if ($topic) { $title .= lang($topic); } else { $title .= lang("index"); }
 
-header('Content-type: text/html; charset=$charset');
+header("Content-type: text/html; charset=$charset");
 echo "<HTML><HEAD>\n";
 echo " <TITLE>$title</TITLE>\n";
 echo " <meta http-equiv='Content-Type' content='text/html; charset=$charset'>\n";
@@ -300,12 +120,12 @@ if ( file_exists($pvp->tpl_dir . "/top.js") )
   echo " <script type='text/javascript' language='JavaScript' src='". str_replace($base_path,$base_url,$pvp->tpl_dir) . "/top.js'></script>\n";
 echo "</HEAD><BODY>\n";
 
-$pm = new pagemaker;
+$pm = new faq($pvp->tpl_dir,"help.tpl",0);
 $pm->set_nav("back","<A HREF=\"JavaScript:history.back()\">" . lang("back") . "</A>");
 if ( !$force_en && $pvp->preferences->get("lang")!="en" )
-  $pm->set_nav("force","<A HREF='$PHP_SELF?topic=$topic&force_en=1'><IMG SRC='$base_url/templates/default/images/english.jpg' BORDER='0' ALT='English'></A>");
-$pm->set_nav("index","<A HREF=\"$PHP_SELF\">" . lang("index") . "</A>");
-$pm->set_nav("close","<A HREF=\"JavaScript:window.close()\">" . lang("close") . "</A>");
+  $pm->set_nav("force","<A HREF='".$_SERVER["PHP_SELF"]."?topic=$topic&force_en=1'><IMG SRC='$base_url/templates/default/images/english.jpg' BORDER='0' ALT='English'></A>");
+$pm->set_nav("index","<A HREF='".$_SERVER["PHP_SELF"]."\'>" . lang("index") . "</A>");
+$pm->set_nav("close","<A HREF='JavaScript:window.close()'>" . lang("close") . "</A>");
 $pm->set_nav("btn_index",lang("index"));
 $pm->set_nav("btn_back",lang("back"));
 $pm->set_nav("btn_close",lang("close"));
@@ -318,7 +138,8 @@ if ($topic) { // display specific help page
       $help->file = dirname(__FILE__) . "/en/no_topic.inc";
     }
   }
-  $pm->make_page(lang($topic),$help->file);
+  $pm->parseInput($help->file,lang($topic));
+  $pm->parseOutput();
 } else { // display help index
   include("help_topics.php");
   $pm->t->set_var("listtitle",lang("index"));

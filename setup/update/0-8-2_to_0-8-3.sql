52cb8991793b92ea758ed1a42be6b978fa6ab810
Izzy <izzysoft@qumran.org>
2007-01-15 07:06:36 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@2391 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

--- setup/update/0-8-2_to_0-8-3.sql
+++ setup/update/0-8-2_to_0-8-3.sql
@@ -1,9 +1,12 @@
 # ========================================================
-# Updating Database for phpVideoPro from v0.8.2 to v0.8.3
+# Updating Database for phpVideoPro from v0.8.2 to v0.8.4
 # ========================================================
 
 # prepare default lang update
 DELETE FROM lang WHERE lang='en';
 
 # version update
-UPDATE pvp_config SET value='0.8.3' WHERE name='version';
+UPDATE pvp_config SET value='0.8.4' WHERE name='version';
+
+# New IMDB TX option
+INSERT INTO pvp_options (name,value) VALUES ('imdb_tx','imdb_tx_rating');

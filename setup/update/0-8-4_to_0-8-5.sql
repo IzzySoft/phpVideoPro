e34bae3f013271bc5f3f0cb9abb231909ca14309
Izzy <izzysoft@qumran.org>
2007-02-07 19:42:09 +0000

    + added some more cd/dvd labels
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@2411 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

--- setup/update/0-8-4_to_0-8-5.sql
+++ setup/update/0-8-4_to_0-8-5.sql
@@ -10,7 +10,7 @@ UPDATE pvp_config SET value='0.8.5' WHERE name='version';
 
 # New CD/DVD labels
 INSERT INTO label_forms VALUES
-  (3,'Standard','CD/DVD Etikett','CD/DVD Etikett',3,3,11.9,11.9,11.9,11.9,1.1,
+  (3,'Memorex','CD/DVD Label','CD/DVD Label',3,3,11.9,11.9,11.9,11.9,1.1,
    2.4,1,1,1);
 INSERT INTO eps_templates VALUES
   (27,3,'Action','action_cddvd.eps','common_cddvd.ps',0,0,468,474);
@@ -22,4 +22,19 @@ INSERT INTO eps_templates VALUES
   (30,3,'SciFi','sf_cddvd.eps','common_cddvd.ps',0,0,468,474);
 INSERT INTO eps_templates VALUES
   (31,3,'Simple','simple_cddvd.eps','common_cddvd.ps',0,0,468,474);
-
+INSERT INTO eps_templates VALUES
+  (32,3,'Marmor','marmor_cddvd.eps','common_cddvd.ps',0,0,468,474);
+INSERT INTO eps_templates VALUES
+  (33,3,'Sky','sky_cddvd.eps','common_cddvd.ps',0,0,468,474);
+INSERT INTO eps_templates VALUES
+  (34,3,'Water','water_cddvd.eps','common_cddvd.ps',0,0,468,474);
+INSERT INTO eps_templates VALUES
+  (35,3,'Pool','pool_cddvd.eps','common_cddvd.ps',0,0,468,474);
+INSERT INTO eps_templates VALUES
+  (36,3,'Electronic','electronic_cddvd.eps','common_cddvd.ps',0,0,468,474);
+INSERT INTO eps_templates VALUES
+  (37,3,'Citrus','citrus_cddvd.eps','common_cddvd.ps',0,0,468,474);
+INSERT INTO eps_templates VALUES
+  (38,3,'World','world_cddvd.eps','common_cddvd.ps',0,0,468,474);
+INSERT INTO eps_templates VALUES
+  (39,3,'Clouds','clouds_cddvd.eps','common_cddvd.ps',0,0,468,474);

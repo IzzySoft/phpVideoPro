c71a7bf2c4b56a220608aaedf70506e4149b5653
Izzy <izzysoft@qumran.org>
2010-04-25 12:46:07 +0000

    ! fix in update script
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/trunk@2625 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

--- setup/update/0-9-5_to_0-9-6.sql
+++ setup/update/0-9-5_to_0-9-6.sql
@@ -2,8 +2,5 @@
 # Updating Database for phpVideoPro from v0.9.5 to v0.9.6
 # ========================================================
 
-# prepare default lang update
-DELETE FROM pvp_lang WHERE lang='en';
-
 # version update
 UPDATE pvp_config SET value='0.9.6' WHERE name='version';

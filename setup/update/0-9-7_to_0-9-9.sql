97d45406beb82dbff0b831104ba96545dd78ca9c
Izzy <izzy@qumran.org>
2020-05-02 20:01:05 +0200

    upgrading IMDB URLs in options from http to https

--- setup/update/0-9-7_to_0-9-9.sql
+++ setup/update/0-9-7_to_0-9-9.sql
@@ -2,6 +2,11 @@
 # Updating Database for phpVideoPro from v0.9.7 to v0.9.9
 # ========================================================
 
-# version update
+# add Blu-ray disc
 INSERT INTO pvp_mtypes (name,sname) VALUES 'Blu-ray Disc','BD';
+
+# upgrade IMDB URLs to https
+UPDATE pvp_options SET value=REPLACE(value,'http:','https:');
+
+# version update
 UPDATE pvp_config SET value='0.9.9' WHERE name='version';
